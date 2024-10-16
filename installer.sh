41b1898bf4d6be11dd621c85cd627c866c818b04
Marcus Johansson <30673661+UnconnectedBedna@users.noreply.github.com>
2024-10-16 08:43:33 +0200

    Update installer.sh
    Curl does not want to play nice and wait for user input when using `read`
    
    Adding `< /dev/tty` might work.

--- installer.sh
+++ installer.sh
@@ -53,13 +53,13 @@ for (( i=0; i<${#FILES_2_DOWNLOAD[@]}; i++ )); do
   sourceFile="${FILES_2_DOWNLOAD[$i]}"
   targetDir="${FILES_2_STORE[$i]}"
 
-  echo -n "Downloading $sourceFile from ${DOWNLOAD_REPOSITORY}/${sourceFile}..."
+  echo "Downloading $sourceFile from ${DOWNLOAD_REPOSITORY}/${sourceFile}..."
   http_code=$(curl -w "%{http_code}" -L -s ${DOWNLOAD_REPOSITORY}/${sourceFile} -o $sourceFile)
 
   (( $? )) && { echo "Curl failed"; exit 1; }
   [[ $http_code != 200 ]] && { echo "http request failed with $http_code"; exit 1; }
 
-  echo -n "Installing $sourceFile into ${targetDir}..."
+  echo "Installing $sourceFile into ${targetDir}..."
   # we run bash as root, chown should not be needed
   #chown root:root "${sourceFile}"
   #(( $? )) && { echo "chown of $sourceFile failed"; exit 1; }
@@ -91,7 +91,7 @@ done
 if [ -f ${DIR_ETC}/shrink-backup.conf ]; then
   echo "${DIR_ETC}/shrink-backup.conf already exists"
   while true; do
-    read -n 1 -r -p '!! Do you want to overwrite? [y/n] ' input
+    read -n 1 -r -p '!! Do you want to overwrite? [y/n] ' input < /dev/tty
     case $input in
       [Yy]) echo -e '\nOverwriting...'; mv "${DIR_ETC}"/"${EXCLUDE_FILE}" "${DIR_ETC}"/shrink-backup.conf; break;;
       [Nn]) echo -e '\nKeeping old file...'; rm "${DIR_ETC}"/"${EXCLUDE_FILE}"; break;;
