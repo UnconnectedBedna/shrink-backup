1a4b0c9b0cc385d0ad3fd1a59b2df7533b35f2c9
Marcus Johansson <30673661+UnconnectedBedna@users.noreply.github.com>
2024-10-16 08:18:11 +0200

    Update installer.sh
    This script will be in a fork of its own (install) in the future instead of main and testing.
    
    Bash indentation is 2...
    
    Out-commented some code not necessary and made comments, this will be removed later. (mostly for the framps that created the installer in the first place)
    
    Added a "sudo check", or the script will exit.
    
    Made changes to renaming the config file later in script, checking if a previous file is already exists and asks for overwrite confirmation.

--- installer.sh
+++ installer.sh
@@ -1,80 +1,105 @@
 #!/bin/bash
 #
 # Simple installer for shrink-backup
+# Special thanks to https://github.com/framps
 #
 # Command to use to install shrink-backup:
-#	   curl https://raw.githubusercontent.com/UnconnectedBedna/shrink-backup/main/installer.sh | sudo bash
+#	   curl https://raw.githubusercontent.com/UnconnectedBedna/shrink-backup/install/installer.sh | sudo bash
 #
+
+# Check if script is run as root
+if [ "$EUID" -ne 0 ]; then
+  echo 'THIS SCRIPT MUST BE RUN AS ROOT! (WITH SUDO)'
+  exit
+fi
 
 set -uo pipefail
 
-# Has to be updated when PR is accepted
-readonly REPOSITORY="framps" # UnconnectedBedna
-readonly BRANCH="install" # testing
+readonly REPOSITORY="UnconnectedBedna"
+readonly BRANCH="testing" # change to install after testing and creation of fork
 
 readonly PACKAGE_FILE="shrink-backup"
+readonly EXCLUDE_FILE="exclude.txt"
 readonly LICENSE_FILE="LICENSE"
 readonly README_FILE="README.md"
-readonly DIR_DOC="/usr/share/doc/$PACKAGE_FILE"
 readonly DIR_EXE="/usr/local/sbin"
 readonly DIR_ETC="/usr/local/etc"
+readonly DIR_DOC="/usr/share/doc/$PACKAGE_FILE"
 readonly DOWNLOAD_REPOSITORY="https://raw.githubusercontent.com/$REPOSITORY/$PACKAGE_FILE/$BRANCH"
-readonly FILES_2_DOWNLOAD=("$PACKAGE_FILE" "${PACKAGE_FILE}.conf" "$LICENSE_FILE" "$README_FILE")
+readonly FILES_2_DOWNLOAD=("$PACKAGE_FILE" "$EXCLUDE_FILE" "$LICENSE_FILE" "$README_FILE")
 readonly FILES_2_STORE=("$DIR_EXE" "$DIR_ETC" "$DIR_DOC" "$DIR_DOC")
 readonly TMP_DIR=$(mktemp -d)
 
 function cleanup() {
-	local exitStatus=$?
-	cd $pwd
-	rmdir $TMP_DIR &>/dev/null
-	if (( $exitStatus > 0 )); then
-		echo "Installation of $PACKAGE_FILE failed with rc $exitStatus"
-	else
-		echo "$PACKAGE_FILE successfully installed"
-	fi
-	exit $exitStatus
+  local exitStatus=$?
+  cd $pwd
+  rmdir $TMP_DIR &>/dev/null
+  if (( $exitStatus > 0 )); then
+    echo "Installation of $PACKAGE_FILE failed with rc $exitStatus"
+  else
+    echo "$PACKAGE_FILE successfully installed"
+  fi
+  exit $exitStatus
 }
 
 pwd=$PWD
 trap "cleanup" SIGINT SIGTERM EXIT
 cd $TMP_DIR
 
-echo "Installing $PACKAGE_FILE ..."
+echo "Installing ${PACKAGE_FILE}..."
 
 for (( i=0; i<${#FILES_2_DOWNLOAD[@]}; i++ )); do
 
-	sourceFile="${FILES_2_DOWNLOAD[$i]}"
-	targetDir="${FILES_2_STORE[$i]}"
+  sourceFile="${FILES_2_DOWNLOAD[$i]}"
+  targetDir="${FILES_2_STORE[$i]}"
 
-	echo -n "Downloading $sourceFile from $DOWNLOAD_REPOSITORY/$sourceFile ... "
-	http_code=$(curl -w "%{http_code}" -L -s $DOWNLOAD_REPOSITORY/$sourceFile -o $sourceFile)
+  echo -n "Downloading $sourceFile from ${DOWNLOAD_REPOSITORY}/${sourceFile}..."
+  http_code=$(curl -w "%{http_code}" -L -s ${DOWNLOAD_REPOSITORY}/${sourceFile} -o $sourceFile)
 
-	(( $? )) && { echo "Curl failed"; exit 1; }
-	[[ $http_code != 200 ]] && { echo "http request failed with $http_code"; exit 1; }
-	echo "done"
+  (( $? )) && { echo "Curl failed"; exit 1; }
+  [[ $http_code != 200 ]] && { echo "http request failed with $http_code"; exit 1; }
 
-	echo -n "Installing $sourceFile into $targetDir ... "
-	sudo chown root:root "${sourceFile}"
-	(( $? )) && { echo "chown of $sourceFile failed"; exit 1; }
+  echo -n "Installing $sourceFile into ${targetDir}..."
+  # we run bash as root, chown should not be needed
+  #chown root:root "${sourceFile}"
+  #(( $? )) && { echo "chown of $sourceFile failed"; exit 1; }
 
-	sudo chmod 755 "${sourceFile}"
-	(( $? )) && { echo "chmod of $sourceFile failed"; exit 1; }
+  # I dont think all files should be 755
+  #chmod 755 "${sourceFile}"
+  #(( $? )) && { echo "chmod of $sourceFile failed"; exit 1; }
 
-	if [[ "$sourceFile" == "$PACKAGE_FILE" ]]; then # existing executable bit in github is not reflected by curl
-		sudo chmod +x "$sourceFile"
-		(( $? )) && { echo "chmod of $sourceFile failed"; exit 1; }
-		sed --follow-symlinks -i -E "s/^(INSTALL_METHOD)=.+$/\1=\'curl\'/" "$sourceFile"
-		(( $? )) && { echo "sed of $sourceFile failed"; exit 1; }
-	elif [[ ! -d "$DIR_DOC" ]] && [[ "$sourceFile" == "$LICENSE_FILE" || "$sourceFile" == "$README_FILE" ]] ; then # create LICENSE directory
-		sudo mkdir -p "$DIR_DOC"
-		(( $? )) && { echo "mkdir of $DIR_DOC failed"; exit 1; }
-		sudo chown root:root "${DIR_DOC}/.."
-		(( $? )) && { echo "chown of ${DIR_SOC}/.. failed"; exit 1; }
-	fi
-	sudo mv "${sourceFile}" "${targetDir}"
-	(( $? )) && { echo "mv of $sourceFile failed"; exit 1; }
-	echo "done"
+  # Existing execution bit in github is not reflected by curl
+  if [[ "$sourceFile" == "$PACKAGE_FILE" ]]; then
+    chmod +x "$sourceFile"
+    (( $? )) && { echo "chmod of $sourceFile failed"; exit 1; }
+    sed --follow-symlinks -i -E "s/^(INSTALL_METHOD)=.+$/\1=\'curl\'/" "$sourceFile"
+    (( $? )) && { echo "sed of $sourceFile failed"; exit 1; }
+  # Create LICENSE directory
+  elif [[ ! -d "$DIR_DOC" ]] && [[ "$sourceFile" == "$LICENSE_FILE" || "$sourceFile" == "$README_FILE" ]] ; then
+    mkdir -p "$DIR_DOC"
+    (( $? )) && { echo "mkdir of $DIR_DOC failed"; exit 1; }
+    # Again, we are running bash as sudo...
+    #chown root:root "${DIR_DOC}/.."
+    #(( $? )) && { echo "chown of ${DIR_SOC}/.. failed"; exit 1; }
+  fi
+  mv "$sourceFile" "$targetDir"
+  (( $? )) && { echo "mv of $sourceFile failed"; exit 1; }
 
 done
+
+# Renaming exclude.txt to shrink-backup.conf since it is now located in /usr/local/etc
+if [ -f ${DIR_ETC}/shrink-backup.conf ]; then
+  echo "${DIR_ETC}/shrink-backup.conf already exists"
+  while true; do
+    read -n 1 -r -p '!! Do you want to overwrite? [y/n] ' input
+    case $input in
+      [Yy]) echo -e '\nOverwriting...'; mv "${DIR_ETC}"/"${EXCLUDE_FILE}" "${DIR_ETC}"/shrink-backup.conf; break;;
+      [Nn]) echo -e '\nKeeping old file...'; rm "${DIR_ETC}"/"${EXCLUDE_FILE}"; break;;
+      *) echo -e "\nERROR! Please enter 'y/Y' or 'n/N'";;
+    esac
+  done
+else
+  mv "${DIR_ETC}"/"${EXCLUDE_FILE}" "${DIR_ETC}"/shrink-backup.conf
+fi
 
 exit 0
